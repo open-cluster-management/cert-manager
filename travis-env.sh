08041d4dd515191aea09a4186a7a221297d5f7b3
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-08-21 15:57:25 -0400

    Added the additional check if the build is a pull request.

--- travis-env.sh
+++ travis-env.sh
@@ -1,15 +1,19 @@
 # Release Tag
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    RELEASE_TAG=latest
-    ARTIFACTORY_IMAGE_REPO=hyc-cloud-private-integration-docker-local
-    ARTIFACTORY_NAMESPACE=ibmcom
-    ARTIFACTORY_RELEASE_TAG=0.3.0
-    if [ "$OS" = "rhel7" ]; then
-        ARTIFACTORY_RELEASE_TAG="${ARTIFACTORY_RELEASE_TAG}-rhel"
+    if ! [ "$TRAVIS_BRANCH" = "pull_request" ]; then
+        RELEASE_TAG=latest
+        ARTIFACTORY_IMAGE_REPO=hyc-cloud-private-integration-docker-local
+        ARTIFACTORY_NAMESPACE=ibmcom
+        ARTIFACTORY_RELEASE_TAG=0.3.0
+
+        if [ "$OS" = "rhel7" ]; then
+            ARTIFACTORY_RELEASE_TAG="${ARTIFACTORY_RELEASE_TAG}-rhel"
+        fi
+
+        export ARTIFACTORY_IMAGE_REPO="$ARTIFACTORY_IMAGE_REPO"
+        export ARTIFACTORY_NAMESPACE="$ARTIFACTORY_NAMESPACE"
+        export ARTIFACTORY_RELEASE_TAG="$ARTIFACTORY_RELEASE_TAG"
     fi
-    export ARTIFACTORY_IMAGE_REPO="$ARTIFACTORY_IMAGE_REPO"
-    export ARTIFACTORY_NAMESPACE="$ARTIFACTORY_NAMESPACE"
-    export ARTIFACTORY_RELEASE_TAG="$ARTIFACTORY_RELEASE_TAG"
 else
     if [ "$TRAVIS_TAG" != "" ]; then
         RELEASE_TAG="${TRAVIS_TAG#v}"
