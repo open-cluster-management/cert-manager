c2ad1d5a537685aa441448888b85e6e5bdcf61b7
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-10-11 12:04:33 -0400

    Testing master tags.

--- travis-env.sh
+++ travis-env.sh
@@ -1,14 +1,14 @@
 # Release Tag
 if [ "$TRAVIS_BRANCH" = "master" ] && ! [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
     RELEASE_TAG=latest
-    ARTIFACTORY_IMAGE_REPO=hyc-cloud-private-integration-docker-local
+    #ARTIFACTORY_IMAGE_REPO=hyc-cloud-private-integration-docker-local
     ARTIFACTORY_NAMESPACE=ibmcom
     ARTIFACTORY_RELEASE_TAG=0.5.0
     if [ "$OS" = "rhel7" ]; then
         ARTIFACTORY_RELEASE_TAG="${ARTIFACTORY_RELEASE_TAG}-rhel"
     fi
 
-    export ARTIFACTORY_IMAGE_REPO="$ARTIFACTORY_IMAGE_REPO"
+    #export ARTIFACTORY_IMAGE_REPO="$ARTIFACTORY_IMAGE_REPO"
     export ARTIFACTORY_NAMESPACE="$ARTIFACTORY_NAMESPACE"
     export ARTIFACTORY_RELEASE_TAG="$ARTIFACTORY_RELEASE_TAG"
 else
@@ -23,6 +23,15 @@ else
         IMAGE_VERSION="${TRAVIS_IMAGE_VERSION}"
         export IMAGE_VERSION="$TRAVIS_IMAGE_VERSION"
     fi
+    #### REMOVE AFTER TESTING ####
+    IMAGE_VERSION=0.5.0
+    ARTIFACTORY_RELEASE_TAG=latest
+    if [ "$OS" = "rhel7" ]; then
+        ARTIFACTORY_RELEASE_TAG="${ARTIFACTORY_RELEASE_TAG}-rhel"
+    fi
+    export ARTIFACTORY_RELEASE_TAG="$ARTIFACTORY_RELEASE_TAG"
+    export IMAGE_VERSION="$IMAGE_VERSION"
+    #### REMOVE ####
 fi
 export RELEASE_TAG="$RELEASE_TAG"
 
