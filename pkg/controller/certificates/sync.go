ecea667880d2b712e45fda8939363c57f88289ec
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2019-02-28 13:46:42 -0500

    Changed the import.

--- pkg/controller/certificates/sync.go
+++ pkg/controller/certificates/sync.go
@@ -25,7 +25,7 @@ import (
 	"strings"
 	"time"
 
-	v1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
