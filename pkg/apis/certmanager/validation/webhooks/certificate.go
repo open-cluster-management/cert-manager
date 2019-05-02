4dd15302c19c7a18403b8c3524669a7ea3f80506
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2019-05-02 14:16:50 -0400

    Removed debug logging.

--- pkg/apis/certmanager/validation/webhooks/certificate.go
+++ pkg/apis/certmanager/validation/webhooks/certificate.go
@@ -74,12 +74,8 @@ func (c *CertificateAdmissionHook) Validate(admissionSpec *admissionv1beta1.Admi
 		return status
 	}
 
-	klog.Infof("%s", obj.Spec.IssuerRef.Kind)
-	klog.Infof("------------- USER INFO FOR %s --------------", obj.ObjectMeta.Name)
-	findUser(admissionSpec)
 	authorized := allowed(admissionSpec, obj)
 	if !authorized {
-		klog.Info("UNAUTHORIZED")
 		status.Allowed = false
 		status.Result = &metav1.Status{
 			Status: metav1.StatusFailure, Code: http.StatusUnauthorized, Reason: metav1.StatusReasonUnauthorized,
@@ -103,13 +99,6 @@ func (c *CertificateAdmissionHook) Validate(admissionSpec *admissionv1beta1.Admi
 	return status
 }
 
-func findUser(admissionSpec *admissionv1beta1.AdmissionRequest) {
-	klog.Infof("USERINFO: %v", admissionSpec.UserInfo)
-	klog.Infof("USERNAME: %s", admissionSpec.UserInfo.Username)
-	klog.Infof("UID: %s", admissionSpec.UserInfo.UID)
-	klog.Infof("GROUPS: %v", admissionSpec.UserInfo.Groups)
-}
-
 func allowed(request *admissionv1beta1.AdmissionRequest, crt *v1alpha1.Certificate) bool {
 	issuerKind := crt.Spec.IssuerRef.Kind
 	username := request.UserInfo.Username
@@ -119,32 +108,16 @@ func allowed(request *admissionv1beta1.AdmissionRequest, crt *v1alpha1.Certifica
 		return false
 	}
 	if issuerKind == "ClusterIssuer" {
-		
 		if uid.Fragment != "" {
+			// Check if this user is the default cluster admin
 			if value, ok := os.LookupEnv("DEFAULT_ADMIN"); ok {
-				klog.Infof("Value: %s\nFragment: %s", value, uid.Fragment)
 				value = strings.TrimSpace(value)
 				if uid.Fragment == value {
 					return true
 				}
 			}
-			// Make api call to iam to check user id
-			
-/* 			accessToken, err := getAccessToken()
-			if err != nil {
-				klog.Infof("Error occurred getting the access token: %s", err.Error())
-				return false
-			}
-			highestRole, err := getHighestRole(accessToken, uid.Fragment)
-			if err != nil {
-				klog.Infof("Error occurred getting the highest role for user: %s", err.Error())
-				return false
-			}
-			klog.Infof("Highest role: %s", highestRole)
-			if highestRole == "ClusterAdmin" {
-				return true
-			} */
 		}
+		// If the user is in systems:master group (ClusterAdmin)
 		groups := request.UserInfo.Groups
 		for _, group := range groups {
 			if group == "system:serviceaccounts:cert-manager" || group == "system:masters" {
@@ -154,85 +127,4 @@ func allowed(request *admissionv1beta1.AdmissionRequest, crt *v1alpha1.Certifica
 		return false
 	}
 	return true
-}
-
-func getAccessToken() (string, error) {
-	file := "/etc/cfc/api-key"
-	apiKeyFile, err := ioutil.ReadFile(file)
-	if err != nil {
-		klog.Infof("Error occurred reading api key file: %s", err.Error())
-		return "", err
-	}
-	apiKey := string(apiKeyFile)
-	// Use api key to get access token
-	management_url := "https://9.46.73.170:8443"
-	accessTokenApi := "iam-token/oidc/token"
-	data := url.Values{}
-	data.Set("grant_type", "urn:ibm:params:oauth:grant-type:apikey")
-	data.Add("apikey", apiKey)
-	data.Add("response_type", "cloud_iam")
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{
-		Transport: tr,
-		Timeout:   10 * time.Second}
-	reqBody := []byte(data.Encode())
-	request, err := http.NewRequest("POST", fmt.Sprintf("%s/%s", management_url, accessTokenApi), ioutil.NopCloser(bytes.NewReader(reqBody)))
-	if err != nil {
-		klog.Infof("Error occurred creating a new request: %s", err.Error())
-		return "", err
-	}
-	request.Header.Add("Accept", "application/json")
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	res, err := client.Do(request)
-	if err != nil {
-		klog.Infof("Error occurred sending request: %s", err.Error())
-		return "", err
-	}
-	if res.StatusCode > 299 {
-		return "", fmt.Errorf("%s\n%s", "StatusCode from request is not 200, ", res.Status)
-	}
-	defer res.Body.Close()
-	out, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		klog.Infof("Error retrieving OIDC Key: %v", err)
-		return "", err
-	}
-	var tokenResponse OIDCTokenResponse
-	json.Unmarshal(out, &tokenResponse)
-	return tokenResponse.AccessToken, nil
-}
-
-func getHighestRole(token string, uid string) (string, error) {
-	management_url := "https://9.46.73.170:8443"
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{
-		Transport: tr,
-		Timeout:   60 * time.Second}
-	api := fmt.Sprintf("/idmgmt/identity/api/v1/teams/roleMappings?userid=%s", uid)
-	request, err := http.NewRequest("GET", fmt.Sprintf("%s%s", management_url, api), nil)
-	if err != nil {
-		klog.Infof("Error creating request for highest role: %s", err.Error())
-		return "", err
-	}
-	request.Header.Add("Accept", "application/json")
-	request.Header.Add("Authorization", "Bearer " + token)
-	klog.Infof("URL: %s", fmt.Sprintf("%s%s", management_url, api))
-	klog.Infof("auth bearer: %s", token)
-	res, err := client.Do(request)
-	if err != nil { 
-		klog.Infof("Error occurred sending request to get highest role: %s", err.Error())
-		return "", err
-	}
-	out, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		klog.Infof("Error retrieving highest role: %s", err.Error())
-		return "", err
-	}
-	var highestRole string
-	json.Unmarshal(out, &highestRole)
-	return highestRole, nil
 }
