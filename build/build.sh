0e2424b88e6bfa445d6fa334b629a511374128f9
Gus Parvin <gparvin@us.ibm.com>
2020-02-10 14:14:38 -0500

    update build to copy license files

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,9 @@ export GOARCH=$(go env GOARCH)
 for PROJECT in `ls hack/build/dockerfiles`; do
 	export PROJECT
 	make go-binary
+	cp LICENSE hack/build/dockerfiles/$PROJECT
+	cp License.txt hack/build/dockerfiles/$PROJECT
+	cp packages.yaml hack/build/dockerfiles/$PROJECT
 	export DOCKER_IMAGE=cert-manager-$PROJECT
 	make docker/build
 done
