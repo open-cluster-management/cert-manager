71486410fc00e46ade457568cccc500d831ff438
Gus Parvin <gparvin@us.ibm.com>
2020-03-17 17:04:24 -0400

    add support for owner references for just the ACM namespace

--- pkg/controller/certificates/sync.go
+++ pkg/controller/certificates/sync.go
@@ -490,7 +490,11 @@ func (c *controller) updateSecret(ctx context.Context, crt *v1alpha1.Certificate
 	// if it is a new resource
 	if secret.SelfLink == "" {
 		if c.addOwnerReferences {
-			secret.SetOwnerReferences(append(secret.GetOwnerReferences(), ownerRef(crt)))
+			// ACM Uninstall support
+			ownedNS := os.Getenv("OWNED_NAMESPACE", "")
+			if ownedNS == "" || namespace == ownedNS {
+				secret.SetOwnerReferences(append(secret.GetOwnerReferences(), ownerRef(crt)))
+			}
 		}
 		secret, err = c.kClient.CoreV1().Secrets(namespace).Create(secret)
 	} else {
