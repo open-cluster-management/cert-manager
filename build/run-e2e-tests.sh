ab92fb5f71d929c22df654de9f0f1e919c7dff34
Gus Parvin <gparvin@redhat.com>
2020-09-09 12:22:39 -0400

    initiate a rebuild to pick up latest ubi image and updates

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -2,4 +2,8 @@
 set -e
 
 export DOCKER_IMAGE_AND_TAG=${1}
-# make docker/run
+# create kind cluster
+# deploy the cert-manager controller
+# pull down the cert-manager-test-automation repo
+# cd to that repo
+# make test:adoption
